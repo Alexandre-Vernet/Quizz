03c972da8a1afc96c3a599d523e37b876175d413
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-01-23 17:17:38 +0100

    Generate questions dynamically

--- app/src/main/java/com/ynov/vernet/quizz/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/quizz/MainActivity.java
@@ -1,26 +1,46 @@
 package com.ynov.vernet.quizz;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
-    TextView textViewQuestion;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     Button[] btnAnswer = new Button[4];
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewQuestion = findViewById(R.id.textViewQuestion);
         btnAnswer[0] = findViewById(R.id.btnAnswer_1);
         btnAnswer[1] = findViewById(R.id.btnAnswer_2);
         btnAnswer[2] = findViewById(R.id.btnAnswer_3);
         btnAnswer[3] = findViewById(R.id.btnAnswer_4);
 
-        textViewQuestion.setText(new Questions().askQuestion());
+        // Implement buttons
+        for (int i = 0; i < 4; i++)
+            btnAnswer[i].setOnClickListener(this);
+
+        // Generate questions
+        new Questions(this, this).askQuestion();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnAnswer_1:
+                Toast.makeText(this, "coucouuu", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btnAnswer_2:
+                Toast.makeText(this, "coucou 2", Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
--- app/src/main/java/com/ynov/vernet/quizz/Questions.java
+++ app/src/main/java/com/ynov/vernet/quizz/Questions.java
@@ -1,21 +1,79 @@
 package com.ynov.vernet.quizz;
 
+import android.app.Activity;
+import android.content.Context;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
 
 public class Questions {
+    Context context;
+    Activity activity;
+
+    TextView textViewQuestion;
+    Button[] btnAnswer = new Button[4];
+
+    // Debug
     private static final String TAG = "Questions";
 
-    public String askQuestion() {
-        int randomQuestion = ThreadLocalRandom.current().nextInt(0, 10);
+    Questions(Context context, Activity activity) {
+        this.context = context;
+        this.activity = activity;
 
-        String question = "Quelle est ma date de naissance ?";
-        String answer1 = "1998";
-        String answer2 = "1999";
-        String answer3 = "2000";
-        String answer4 = "2001";
+        textViewQuestion = activity.findViewById(R.id.textViewQuestion);
+        btnAnswer[0] = activity.findViewById(R.id.btnAnswer_1);
+        btnAnswer[1] = activity.findViewById(R.id.btnAnswer_2);
+        btnAnswer[2] = activity.findViewById(R.id.btnAnswer_3);
+        btnAnswer[3] = activity.findViewById(R.id.btnAnswer_4);
+    }
 
-        return question;
+    public void askQuestion() {
+
+        String question = "";
+        String[] answer = new String[4];
+
+        // Generate random question
+        Random random = new Random();
+        int randomQuestion = random.nextInt(3 - 0) + 0;
+        Log.d(TAG, "askQuestion: " + randomQuestion);
+
+        switch (randomQuestion) {
+            case 0:
+                question = "Question";
+                answer[0] = "answer0";
+                answer[1] = "answer1";
+                answer[2] = "answer2";
+                answer[3] = "answer3";
+                break;
+            case 1:
+                question = "In what year was Google launched on the web?";
+                answer[0] = "2001";
+                answer[1] = "1999";
+                answer[2] = "1998";
+                answer[3] = "L'an 1";
+                break;
+            case 2:
+                question = "What does \"FTP\" stand for in the computer and internet world?";
+                answer[0] = "Fa Ta Pa";
+                answer[1] = "Force Tripe Prout";
+                answer[2] = "File Transfer Protocol";
+                answer[3] = "La réponse D";
+                break;
+            default:
+                question = "What is the country top-level domain of Belgium?";
+                answer[0] = ".fr";
+                answer[1] = "The .be domain";
+                answer[2] = ".com";
+                answer[3] = ".lo";
+                break;
+        }
+
+        // Display questions
+        textViewQuestion.setText(question);
+        for (int i = 0; i < 4; i++)
+            btnAnswer[i].setText(answer[i]);
+
     }
 }
