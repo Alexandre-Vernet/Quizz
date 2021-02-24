9b527463359006f9733d492a8b53b7963a84dd7b
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-02-24 21:03:10 +0100

    Fixed bug with responsive Grid Layout

--- app/src/main/java/com/ynov/vernet/quizz/CategoryActivity.java
+++ app/src/main/java/com/ynov/vernet/quizz/CategoryActivity.java
@@ -3,6 +3,7 @@ package com.ynov.vernet.quizz;
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.widget.GridLayout;
@@ -15,12 +16,16 @@ public class CategoryActivity extends AppCompatActivity implements View.OnClickL
     GridLayout gridLayout;
     CardView[] cardViews = new CardView[7];
 
+    private static final String TAG = "CategoryActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
 
         gridLayout = findViewById(R.id.gridLayout);
+
+        // Cards
         cardViews[0] = findViewById(R.id.art);
         cardViews[1] = findViewById(R.id.biology);
         cardViews[2] = findViewById(R.id.animals);
@@ -29,23 +34,21 @@ public class CategoryActivity extends AppCompatActivity implements View.OnClickL
         cardViews[5] = findViewById(R.id.computer);
         cardViews[6] = findViewById(R.id.science);
 
+        // Implement cards
         for (int i = 0; i < 7; i++)
             cardViews[i].setOnClickListener(this);
 
+        // Adjust Grid Layout with screen size
         int screenSize = getScreenSize();
 
-        switch (screenSize) {
-            case 1100:
-                gridLayout.setColumnCount(3);
-                break;
-            case 2100:
-                gridLayout.setColumnCount(4);
-                break;
-            case 2400:
-                gridLayout.setColumnCount(5);
-                break;
+        if (screenSize > 1100)
+            gridLayout.setColumnCount(3);
 
-        }
+        if (screenSize > 2100)
+            gridLayout.setColumnCount(4);
+
+        if (screenSize > 2400)
+            gridLayout.setColumnCount(5);
     }
 
     public int getScreenSize() {
@@ -62,18 +65,38 @@ public class CategoryActivity extends AppCompatActivity implements View.OnClickL
         Intent intent = new Intent(getApplicationContext(), MainActivity.class);
 
         switch (v.getId()) {
-            case R.id.computer:
-                intent.putExtra("category", "computer");
-                startActivity(intent);
-                finish();
-                break;
             case R.id.art:
                 intent.putExtra("category", "art");
                 startActivity(intent);
                 finish();
                 break;
+            case R.id.biology:
+                intent.putExtra("category", "biology");
+                startActivity(intent);
+                finish();
+                break;
             case R.id.animals:
                 intent.putExtra("category", "animals");
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.movies:
+                intent.putExtra("category", "movies");
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.geography:
+                intent.putExtra("category", "geography");
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.computer:
+                intent.putExtra("category", "computer");
+                startActivity(intent);
+                finish();
+                break;
+            case R.id.science:
+                intent.putExtra("category", "science");
                 startActivity(intent);
                 finish();
                 break;
