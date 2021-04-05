f22c6e6bcc7e289de3be1577130903cae222baae
Alexandre-Vernet <alexandre.vernet@g-mail.fr>
2021-04-05 19:46:59 +0200

    Adapt question to screen

--- app/src/main/java/com/ynov/vernet/quizz/MainActivity.java
+++ app/src/main/java/com/ynov/vernet/quizz/MainActivity.java
@@ -3,6 +3,7 @@ package com.ynov.vernet.quizz;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -28,6 +29,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewNumberQuestion = findViewById(R.id.textViewNumberQuestion);
         textViewNumberQuestion.setText("Question " + this.countQuestion + " / 10");
         textViewQuestion = findViewById(R.id.textViewQuestion);
+        textViewQuestion.setMovementMethod(new ScrollingMovementMethod());
+
+
 
         // Buttons
         btnAnswer[0] = findViewById(R.id.btnAnswer_0);
